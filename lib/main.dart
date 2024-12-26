ff984e58fd38680f313734669cd5c146ecb85109
Chris Norman <chris.norman1989@outlook.com>
2024-12-26 16:05:38 +0000

    Use `SoLoudScope`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_audio_games/flutter_audio_games.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'src/screens/gpx_files_screen.dart';
@@ -16,15 +18,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(final BuildContext context) {
-    WidgetsFlutterBinding.ensureInitialized();
-    RendererBinding.instance.ensureSemantics();
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    if (kIsWeb) {
+      WidgetsFlutterBinding.ensureInitialized();
+      RendererBinding.instance.ensureSemantics();
+    }
+    return SoLoudScope(
+      child: MaterialApp(
+        title: 'Audio GPX',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const GpxFilesScreen(),
       ),
-      home: const GpxFilesScreen(),
     );
   }
 }
