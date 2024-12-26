143fb4caff68cc570f9eeeb6df4d51692da033c0
Chris Norman <chris.norman1989@outlook.com>
2024-12-26 16:07:34 +0000

    Change the CMD title after building.

--- build-web.bat
+++ build-web.bat
@@ -1,2 +1,2 @@
 @echo off
-flutter build web --release --base-href="/gpx/" & scp -Cr build\web chris@backstreets.site:gpx
+flutter build web --release --base-href="/gpx/" & scp -Cr build\web chris@backstreets.site:gpx & title CMD
